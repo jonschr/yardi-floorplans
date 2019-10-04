0c6b833974a1bdc87d919114ba2ffc6ae63c75c2
jonschr <jonschr@gmail.com>
2019-10-04 17:21:28 -0500

    Carousel fixes

--- js/floorplancarousel-init.js
+++ js/floorplancarousel-init.js
@@ -1,33 +1,31 @@
-jQuery(document).ready(function( $ ) {
-	
+jQuery(document).ready(function ($) {
+
 	$('.loop-layout-floorplancarousel, .loop-layout-floorplancarousel-detailed').slick({
-			infinite: true,
-			slidesToShow: 3,
-			slidesToScroll: 3,
-			dots: true,
-			arrows: false,
-			responsive: [
-			    {
-					breakpoint: 1000,
-					settings: {
-						slidesToShow: 2,
-						slidesToScroll: 2,
-						centerPadding: '40px',
-						centerMode: true,
-						arrows: false,
-					}
-			    },
-			    {
-					breakpoint: 600,
-					settings: {
-						centerMode: true,
-						centerPadding: '40px',
-						slidesToShow: 1,
-						slidesToScroll: 1,
-						dots: false,
-					}
-			    }
-			]
-		});
-	
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		dots: true,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 1000,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					arrows: false,
+					infinite: true,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: true,
+					infinite: true,
+				}
+			}
+		]
+	});
+
 });
